499514bbec8d88dfdc9c6f9e0b751e2894ba990d
oihid <c.ouchene@esi-sba.dz>
2022-10-15 20:15:55 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Doctor app',
+        theme: ThemeData(
+          fontFamily: 'GTWalsheimPro',
+          splashFactory: NoSplash.splashFactory,
+          highlightColor: Colors.transparent,
+          primarySwatch: Colors.blue,
+        ),
+        home:,
+    );
+  }
+}
+
